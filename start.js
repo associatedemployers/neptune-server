3a28eeb418d984916fc3799e49c44cd6ee66ef8f
James-Mac <james@aehr.org>
2014-09-09 11:42:50 -0600

    :lipstick: & Performance Monitor

--- start.js
+++ start.js
@@ -1,25 +1,42 @@
 var server = require('./apiserver'),
-	nodemailer = require('nodemailer'),
-	integrations = require('./routes/external-integrations');
+    nodemailer = require('nodemailer'),
+    PerformanceMonitor = require('./util/performance'),
+    integrations = require('./routes/external-integrations');
+
+var monitor = new PerformanceMonitor({
+	to: [
+		'james@aehr.org',
+		'greg@aehr.org'
+	],
+	alertFrequency: '1 hour',
+	integrationHook: integrations.slack.api.status
+}).memory(250, 1000000, 'MB', 1000 * 20);
+
 integrations.slack.api.status("is going up.");
+
 server.starthttps(3000);
 server.starthttp(3001);
+
 integrations.slack.api.status("is up.");
 
-process.on('uncaughtException', function (err) {
+process.on('uncaughtException', function ( err ) {
 	console.error(err.stack);
+
 	var transport = nodemailer.createTransport("sendmail");
+
 	transport.sendMail({
-		from: 'notifications@jobjupiter.com',
-		to: 'james@aehr.org',
-		subject: "Uncaught Exception Occurred: " + err.message,
-		text: err.stack
-	}, function (mailerr) {
-		if (mailerr) console.error(mailerr);
+			from:    'notifications@jobjupiter.com',
+			to:      'james@aehr.org',
+			subject: 'Uncaught Exception Occurred: ' + err.message,
+			text:    err.stack
+		}, function ( mailerr ) {
+		if( mailerr ) console.error(mailerr);
+
 		integrations.slack.api.error(err, function () {
 			process.exit(1);
 		});
 	});
+
 });
 
 process.on('SIGINT', function () {
---
+++ util/performance.js
@@ -0,0 +1,115 @@
+/* Performance Monitor */
+
+var moment     = require('moment'),
+    nodemailer = require('nodemailer'),
+    _          = require('lodash');
+
+module.exports = PerformanceMonitor;
+
+/**
+* PerformanceMonitor Constructor
+* 
+* @options {Object}
+* 
+* @return self
+*/
+function PerformanceMonitor ( options ) {
+  this.options = options || {};
+
+  return this;
+}
+
+/**
+* PerformanceMonitor.memory
+* 
+* @limit      {Number} Limit before alert
+* @multiplier {Number} Byte multiplier
+* @check      {Number} Milliseconds in between memory checks
+* 
+* @return interval
+*/
+PerformanceMonitor.prototype.memory = function ( limit, multiplier, multiplierDescription, check ) {
+  var self = this;
+
+  // Defaults
+  limit                 = limit                 || 400;
+  multiplier            = multiplier            || 1000000;       // MB
+  multiplierDescription = multiplierDescription || 'MB';
+  check                 = check                 || 1000 * 60 * 5; // 5min
+
+  var limitM = limit * multiplier;
+
+  return setInterval(function () {
+
+    var freeMemory = os.freemem() * multiplier;
+
+    if( !freeMemory ) {
+      return console.log('PerformanceMonitor :: Unable to get free memory!');
+    }
+
+    // If freeMemory is less than our allowed floor
+    if( freeMemory < limitM && self._shouldSendAlert() ) {
+      var diff = limitM - freeMemory;
+      // Subject
+      var s = 'PerformanceMonitor Alert :: Memory below limit';
+      // Message
+      var m = 'PerformanceMonitor is reporting an operating free memory of ' + 
+              freeMemory + multiplierDescription + ' which is ' + diff + 
+              multiplierDescription + ' below your limit of ' + limitM + 
+              multiplierDescription + '. Report time:' + moment().format('YYYY/MM/DD HH:mm:ss');
+
+      self._sendAlert(s, m, function ( err ) {
+        if( err ) {
+          console.log( err );
+        }
+
+        self.lastSentAlert = new Date();
+      });
+    }
+
+  }, check);
+};
+
+/**
+* PerformanceMonitor._sendAlert
+* 
+* Private
+* 
+* @subject  {String} Alert subject line
+* @msg      {String} Alert body
+* @callback {Function}
+* 
+* @return null
+*/
+PerformanceMonitor.prototype._sendAlert = function ( subject, msg, callback ) {
+  var transport = nodemailer.createTransport("sendmail");
+
+  var mailOptions = {
+    from: 'notifications@jobjupiter.com',
+    subject: subject,
+    text: msg
+  };
+
+  mailOptions.to = ( _.isArray( this.options.to ) ) ? this.options.to.join('; ') : this.options.to;
+
+  transport.sendMail(mailOptions, callback);
+
+  if( this.options.integrationHook && typeof this.options.integrationHook === 'function' ) {
+    this.options.integrationHook( msg );
+  }
+};
+
+/**
+* PerformanceMonitor._shouldSendAlert
+* 
+* Private
+* 
+* @return {Boolean} Alert frequency is not over limit
+*/
+PerformanceMonitor.prototype._shouldSendAlert = function () {
+  var lsam        = moment(this.lastSentAlert),
+      currentTime = moment(),
+      frequency   = this.options.alertFrequency || '1 hour';
+
+  return ( this.lastSentAlert && lsam.isValid() ) ? currentTime.isAfter( lsam.add( frequency ) ) : true;
+};
